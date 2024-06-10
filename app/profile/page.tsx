b6238cf0371dd42c503c6e7b314aeac41fab0614
Saurabh <saurabhthapliyal20011682@gmail.com>
2024-06-10 20:22:49 +0530

    First Commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,37 @@
+import ContestRating from '@/components/profile/ContestRating'
+import ProfileInfo from '@/components/profile/ProfileInfo'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <main className='flex gap-6 py-6 px-20 min-h-screen'>
+      <ProfileInfo />
+
+      <div className='flex flex-col gap-4 rounded-xl  w-full'>
+        
+        <ContestRating />
+
+        <div className='flex gap-4'>
+          <div className='h-52 shadow-md rounded-xl bg-white w-full'>
+
+          </div>
+          <div className='h-52 shadow-md rounded-xl bg-white w-full'>
+
+          </div>
+        </div>
+
+        <div className='h-52 shadow-md rounded-xl bg-white w-full'>
+
+        </div>
+
+        <div className='h-[80vh] shadow-md rounded-xl bg-white w-full'>
+
+        </div>
+        
+      </div>
+      
+    </main>
+  )
+}
+
+export default Profile
---
+++ components/profile/LoginForm.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+const LoginForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      alert("All fields are required");
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const apiUrl = "/api/signup";
+
+      const requestData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      };
+
+      const response = await fetch(apiUrl, requestData);
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to sign up: ${response.status} - ${response.statusText}`
+        );
+      }
+
+      setName("");
+      setEmail("");
+      setPassword("");
+      toast.success("Signup successful");
+
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+      <form
+      onSubmit={handleSignup}
+      className=" flex flex-col w-fit  rounded-xl "
+    >
+      <input
+        disabled={isLoading}
+        className="py-2 px-4 focus:ring-2 ring-amber-300  mb-4 outline-none border rounded-sm"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      
+      <input
+        disabled={isLoading}
+        className="py-2 px-4 focus:ring-2 ring-amber-300 mb-4 outline-none border rounded-sm"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      
+      <button
+        className={`rounded-sm py-2 text-white bg-gradient-to-r from-gray-600 to-gray-500 hover:to-gray-400 ${
+          (name && email && password) && "bg-slate-500"
+        }`}
+        type="submit"
+        disabled={isLoading}
+      >
+        {isLoading ? "..." : "Log In"}
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
